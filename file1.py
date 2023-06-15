5ddb2e10204093772d4996fad58e1f18b9eb92ad
= <luis.chavez@ventamovil.com.mx>
2023-06-15 16:56:25 -0600

    avance nueva funcionalidad terminada

--- file1.py
+++ file1.py
@@ -3,3 +3,9 @@ print("Hello World")
 print("Nuevo cambio")
 
 print("Nueva funcionalidad")
+
+print("Nueva funcionalidad avance 1")
+
+print("Nueva funcionalidad avance 2")
+
+print("Nueva funcionalidad terminada (3)")
